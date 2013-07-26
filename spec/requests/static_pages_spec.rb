c622eaef88e4275a6507f787746b055212f8c68a
MatthewMDavis <mattmdavis@gmail.com>
2013-07-26 07:36:53 -0700

    Add test for smart page title method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,26 +3,46 @@ require 'spec_helper'
 
 describe "StaticPages" do
 	subject { page }
-	let(:base_title) { "The Back 140 Microblog"}
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(title)) }
+  end
   describe "Home Page" do
   	before { visit root_path }
-    it { should have_title(full_title("Home")) }
-    it { should have_content('The Back 140 Microblog') }
+    let(:heading) { 'The Back 140' }
+    let(:title) {'Home'}
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
   describe "Help Page" do
   	before { visit help_path }
-    it { should have_title(full_title("Help")) }
-  	it { should have_content('Help') }
+    let(:heading) { 'Help' }
+    let(:title) { 'Help' }
+    it_should_behave_like "all static pages"
 	end
   describe "About Page" do
   	before { visit about_path }
-    it { should have_title(full_title("About Us")) }
-  	it { should have_content('About Us') }
+    let(:heading) { 'About Us' }
+    let(:title) { 'About Us' }
+    it_should_behave_like "all static pages"
 	end	
   describe "Contact Us" do
   	before { visit contact_path }
-    it { should have_title(full_title("Contact Us")) }
-  	it { should have_content('Contact')	}
+    let(:heading) { 'Contact Us' }
+    let(:title) { 'Contact Us' }
+    it_should_behave_like "all static pages"
   end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_selector('h1', text: 'Contact Us')
+    click_link "Home"
+    expect(page).to have_title(full_title('Home'))
+    click_link "Sign up now!"
+    expect(page).to have_selector('h1', text: 'Sign Up')
+  end  
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,10 @@
-  def full_title(page_title)
-    base_title = 'The Back 140 Microblog'
-    if page_title.empty? || page_title == 'Home'
-      base_title
-    else
-      "#{base_title} | #{page_title}" 
-    end
-  end
+include ApplicationHelper
+
+  # def full_title(page_title)
+  #   base_title = 'The Back 140 Microblog'
+  #   if page_title.empty? || page_title == 'Home'
+  #     base_title
+  #   else
+  #     "#{base_title} | #{page_title}" 
+  #   end
+  # end
