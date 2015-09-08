ad47ae534c685acebc31c9ec8d1cc775a0f2abd4
MatthewMDavis <mattmdavis@gmail.com>
2015-09-08 12:36:17 -0700

    fixed bugs related to switch to rails_helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,31 +5,31 @@ describe "StaticPages" do
 	subject { page }
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
-    it { should have_title(full_title(title)) }
+    it { should have_title(full_title(page_title)) }
   end
   describe "Home Page" do
   	before { visit root_path }
     let(:heading) { 'The Back 140' }
-    let(:title) {'Home'}
+    let(:page_title) { 'Home' }
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
   describe "Help Page" do
   	before { visit help_path }
     let(:heading) { 'Help' }
-    let(:title) { 'Help' }
+    let(:page_title) { 'Help' }
     it_should_behave_like "all static pages"
 	end
   describe "About Page" do
   	before { visit about_path }
     let(:heading) { 'About Us' }
-    let(:title) { 'About Us' }
+    let(:page_title) { 'About Us' }
     it_should_behave_like "all static pages"
 	end	
   describe "Contact Us" do
   	before { visit contact_path }
     let(:heading) { 'Contact Us' }
-    let(:title) { 'Contact Us' }
+    let(:page_title) { 'Contact Us' }
     it_should_behave_like "all static pages"
   end
   it "should have the right links on the layout" do
